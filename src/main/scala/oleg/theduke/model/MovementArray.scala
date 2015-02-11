56fcc520d6ebf9de521434bff4b8b11eb0e72339
OK <o_kozlov@yahoo.com>
2015-02-10 21:56:43 -0800

    movement array print upgrade

--- src/main/scala/oleg/theduke/model/MovementArray.scala
+++ src/main/scala/oleg/theduke/model/MovementArray.scala
@@ -5,46 +5,61 @@ import MovementType._
 /**
  * Represents movements of a tile on one side.
  */
-class MovementArray(val selfOffsetRow : Int, val selfOffsetCol: Int) {
-	
+class MovementArray(val selfOffsetRow: Int, val selfOffsetCol: Int) {
+
 	//init to empty
-	val array = Array.fill[Option[MovementType]](5,5)(None)
-	
+	val array = Array.fill[Option[MovementType]](5, 5)(None)
+
 	/**
 	 * Method is created according to Builder design pattern, so that calls could be chained.
-	 *  
+	 *
 	 * Adds movement by absolute coordinates, starting in lower-left corner, which
-	 * has coordinates (0,0), where first coord is the row index, and second is the 
+	 * has coordinates (0,0), where first coord is the row index, and second is the
 	 * column index.
 	 * TODO - TEST, TEST, TEST!!!
 	 */
-	def addMoveByAbs(row: Int, col: Int, mt: MovementType) : MovementArray = {
+	def addMoveByAbs(row: Int, col: Int, mt: MovementType): MovementArray = {
 		array(row)(col) = Some(mt)
 		return this;
 	}
-	
+
 	/**
 	 * Method is created according to Builder design pattern, so that calls could be chained.
-	 * 
+	 *
 	 * Adds movement by offset from "self" on the board. The offset is specified as (row,col)
 	 * coordinate, and offset from the cell containing the "owner" tile  (tile that owns this
 	 * MovementArray).
 	 * TODO - TEST, TEST, TEST!!!
 	 */
-	def addMoveByOffset(row: Int, col: Int, mt: MovementType) : MovementArray = {
+	def addMoveByOffset(row: Int, col: Int, mt: MovementType): MovementArray = {
 		array(row + selfOffsetRow)(col + selfOffsetCol) = Some(mt)
 		return this;
 	}
-	
+
 	//FIXME fix to print SELF, and also FLIP OUTPUT VERTICALLY (print rows UPSIDE-DOWN)
 	//FIXME-2 ALSO - remove Some(..) from print, print movementtype itself
-	override def toString() : String = {
-		
+	override def toString(): String = {
+
 		val sb = new StringBuilder
-		array.foreach { 
-			x => sb ++= x.mkString(" "); sb ++= "\n" 
+		var row = -1
+		array.reverse.foreach { x =>
+			row += 1
+			var col = -1
+			x.foreach { mt =>
+				col += 1
+				if (row == selfOffsetRow && col == selfOffsetCol) {
+					sb ++= "-ME- "
+				} else {
+					if (mt.isDefined) {
+						sb ++= (mt.get.toString().toUpperCase() + " ");
+					} else {
+						sb ++= (mt + " ");
+					}
+				}
+			}
+			sb ++= "\n"
 		}
-		
+
 		return sb.toString();
 	}
 }
